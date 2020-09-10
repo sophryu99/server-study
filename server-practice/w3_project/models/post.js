4d5b8b182e85da8602d8fc37f5323bbb44824a00
sophryu99 <sehwa_ryu@berkeley.edu>
2020-09-10 23:46:57 +0900

    W3: mission

---
+++ server-practice/w3_project/models/post.js
@@ -0,0 +1,23 @@
+module.exports = [
+    {
+        id:0,
+        title: '류세화',
+        content: 'aaa',
+        nickname: 'A',
+        time: '2020-05-09 21:11:44'
+    },
+    {
+        id:1,
+        title: '라라랄',
+        content: 'bbb',
+        nickname: 'B',
+        time: '2020-05-10 22:11:33'
+    },
+    {
+        id:2,
+        title: '루루룰',
+        content: 'ccc',
+        nickname: 'C',
+        time: '2020-05-11 11:55:08'
+    }
+];
