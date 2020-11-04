029a1bf04f4f92a2c648c7890d4bb7bc952e8323
sophryu99 <sehwa_ryu@berkeley.edu>
2020-11-04 23:52:03 +0900

    Add W4: study materials

---
+++ study-materials/week4/members.sql
@@ -0,0 +1,10 @@
+insert into study.members (name, age, status, address) values ('류세화', 23, 'OB', '교대');
+insert into study.members (name, age, status, address) values ('양재욱', 23, 'OB', '잠원');
+insert into study.members (name, age, status, address) values ('신지혜', 23, 'YB', '수유');
+insert into study.members (name, age, status, address) values ('이수진', 23, 'OB', '화정');
+insert into study.members (name, age, status, address) values ('김수현', 25, 'OB', '강남');
+insert into study.members (name, age, status, address) values ('신연상', 23, 'OB', '한티');
+insert into study.members (name, age, status, address) values ('송정우', 23, 'YB', '판교');
+insert into study.members (name, age, status, address) values ('윤가영', 21, 'OB', '교대');
+
+SELECT * FROM study.members;
